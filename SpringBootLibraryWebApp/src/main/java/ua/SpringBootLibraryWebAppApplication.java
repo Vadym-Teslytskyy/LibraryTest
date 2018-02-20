228f4a12107706d6a665dc489310cc4f17b71333
Vadym <t.vadyk@gmail.com>
2018-02-20 21:52:18 +0200

    Delete import BookRepository line

--- SpringBootLibraryWebApp/src/main/java/ua/SpringBootLibraryWebAppApplication.java
+++ SpringBootLibraryWebApp/src/main/java/ua/SpringBootLibraryWebAppApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import ua.repository.BookRepository;
 import ua.service.BookService;
 
 @SpringBootApplication
