d9ca27fd495c4ee914c4917eb524b45969aff8bb
Vadym <t.vadyk@gmail.com>
2018-02-20 21:10:43 +0200

    Create structuring packedges. Create repository interfaces for all entities. Commented BookRepositoryImpl. Commented toString in Book.class

--- SpringBootLibraryWebApp/src/main/java/ua/SpringBootLibraryWebAppApplication.java
+++ SpringBootLibraryWebApp/src/main/java/ua/SpringBootLibraryWebAppApplication.java
@@ -2,11 +2,15 @@ package ua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import ua.repository.BookRepository;
 
 @SpringBootApplication
 public class SpringBootLibraryWebAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLibraryWebAppApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(SpringBootLibraryWebAppApplication.class, args);
+		System.out.println(run.getBean(BookRepository.class).findAll());
 	}
 }
---
+++ SpringBootLibraryWebApp/src/main/java/ua/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package ua.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ua.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Integer>{
+
+}
